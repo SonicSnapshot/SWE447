60b54bcb17a5398e9277f8c8b24d820bba32794a
SonicSnapshot <peter.r.mata@gmail.com>
2018-02-22 17:53:15 -0800

    Update Solar.js
    merc*10

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -198,7 +198,7 @@ function render() {
   planet.PointMode = false;
   ms.push();
   ms.rotate(data.year*time, [0,0,1]);
-  ms.scale(data.radius*3);
+  ms.scale(data.radius*10);
   ms.translate(data.distance*10,0,0);
   gl.useProgram(planet.program);
   gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
