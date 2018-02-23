43fe96aee42daa69b148543b3a0d6595c5ea0a9f
SonicSnapshot <peter.r.mata@gmail.com>
2018-02-22 17:27:07 -0800

    Update Solar.js
    made moon and mercury normal sized again

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -179,7 +179,7 @@ function render() {
   planet.PointMode = false;
   ms.push();
   ms.rotate(data.year*time, [0,0,1]);
-  ms.scale(data.radius *10);
+  ms.scale(data.radius);
   ms.translate(data.distance*1000,0,0);
   gl.useProgram(planet.program);
   gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
@@ -198,7 +198,7 @@ function render() {
   planet.PointMode = false;
   ms.push();
   ms.rotate(data.year*time, [0,0,1]);
-  ms.scale(data.radius *10);
+  ms.scale(data.radius);
   ms.translate(data.distance*10,0,0);
   gl.useProgram(planet.program);
   gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
