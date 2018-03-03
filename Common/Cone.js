6a3426882cdaa340842791442fa29410b3b5f551
SonicSnapshot <peter.r.mata@gmail.com>
2018-03-02 22:43:27 -0800

    Update Cone.js
    added color

--- Common/Cone.js
+++ Common/Cone.js
@@ -28,20 +28,24 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     //   numComponents field.
     //
     this.positions = { numComponents : 3 };
+    this.colors = {numComponents : 3 };
     
     // Initialize temporary arrays for the Cone's indices and vertex positions
     //
     var positions = [ 0.0, 0.0, 0.0 ];
+    var colors = [1.0, 0.0, 0.0 ];
     var indices = [ 0 ];
     
     for ( var i = 0; i < n; ++i ) {
         theta = i * dTheta;
         positions.push( Math.cos(theta), Math.sin(theta), 0.0 );
+        colors.push(1.0, 0.0, 0.0);
 
         indices.push(n - i);
     }
 
     positions.push( 0.0, 0.0, 1.0 );
+    colors.push(1.0, 0.0, 0.0);
     
     // Close the triangle fan by repeating the first (non-center) point.
     //
@@ -67,6 +71,10 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW );
+    
+    this.colors.buffer = gl.createBuffer();
+    gl.bindBuffer (gl.ARRAY_BUFFER, this.colors.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
 
     this.indices.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
@@ -74,13 +82,28 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
 
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
     gl.enableVertexAttribArray( this.positions.attributeLoc );
+    
+    this.uniforms = {
+            MV : undefined,
+            P : undifined
+    };
+    
+    this.uniforms.MV = gl.getUniformLocation(this.program, "MV");
+    this.uniforms.P = gl.getUniformLocation(this.program, "P");
+    
+    this.MV = mat4();
+    this.P = mat4();
 
     this.render = function () {
         gl.useProgram( this.program );
 
         gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
         gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
-            gl.FLOAT, gl.FALSE, 0, 0 );
+            gl.FLOAT, gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0 );
+        
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+        gl.vertexAttribPointer( this.colors.attributeLoc, this.colors.numComponents,
+            gl.FLOAT, gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0 );
  
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 
@@ -91,6 +114,6 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
         // Draw the cone's top
         //
         var offset = this.indices.count * 2 /* sizeof(UNSIGNED_INT) */;
-        gl.drawElements( gl.POINTS, this.indices.count, gl.UNSIGNED_SHORT, offset );
+        gl.drawElements( gl.TRIANGLE_FAN, this.indices.count, gl.UNSIGNED_SHORT, offset );
     }
 };
