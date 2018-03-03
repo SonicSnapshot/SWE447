4c0d68a51bfef582e2cc46ee073a0cb42acc6103
SonicSnapshot <peter.r.mata@gmail.com>
2018-03-02 23:20:51 -0800

    Update cone.js
    perspective

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -140,7 +140,7 @@ function resize() {
     gl.viewport(0,0,width,height);
     var fovy = 120;
     aspect = width/height;
-    thisCone.P = perpspective(fovy,apsect,near,far);
+    thisCone.P = perspective(fovy,apsect,near,far);
 }
 
 function degToRad(degrees) {
